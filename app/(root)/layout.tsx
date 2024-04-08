5102ccb5e9b11da75d332dd05d4840e8f43ee7d1
ywx <540040588@qq.com>
2024-04-08 17:42:37 +0800

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import React from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { useGetOrCallId } from "@/hooks/useGetCallById";
+import Loader from "@/components/Loader";
+
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetOrCallId(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
+
+export default Meeting;
